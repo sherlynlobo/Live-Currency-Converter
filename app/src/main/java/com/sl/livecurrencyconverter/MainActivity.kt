18560c1807e27ffbbf9de567e14b91cef664908d
sherlynlobo <sherlynlobo26@gmail.com>
2020-10-31 19:39:27 -0400

    Spinner Setup

--- app/src/main/java/com/sl/livecurrencyconverter/MainActivity.kt
+++ app/src/main/java/com/sl/livecurrencyconverter/MainActivity.kt
@@ -3,6 +3,10 @@ package com.sl.livecurrencyconverter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spinnerSetup()
 
     }
     private fun getApiResult()
@@ -44,6 +50,12 @@ class MainActivity : AppCompatActivity() {
                        Log.d("Main", "$conversionRate")
                        Log.d("Main", apiResult)
 
+                       withContext(Dispatchers.Main)
+                       {
+                           val text = ((et_firstConversion.text.toString().toFloat()) * conversionRate).toString()
+                           et_secondConversion?.setText(text)
+                       }
+
                       }
                    catch (e:Exception)
                    {
@@ -52,5 +64,64 @@ class MainActivity : AppCompatActivity() {
                }
             }
         }
+    }
+
+    private fun spinnerSetup()
+    {
+      val spinner: Spinner = findViewById(R.id.spinner_firstConversion)
+        val spinner2: Spinner = findViewById(R.id.spinner_secondConversion)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currencies,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currencies2,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner2.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+               baseCurrency = parent?.getItemAtPosition(position).toString()
+                getApiResult()
+            }
+
+        })
+
+        spinner2.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                convertedToCurrency = parent?.getItemAtPosition(position).toString()
+                getApiResult()
+            }
+
+        })
+
     }
 }
